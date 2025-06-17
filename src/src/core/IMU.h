927c69fdff5b6174ffdc36b306db13e2ce8914a3
Teto <teto75928@gmail.com>
2025-06-17 19:52:16 +0300

    code with classes and a few editing to it

---
+++ src/src/core/IMU.h
@@ -0,0 +1,23 @@
+#ifndef IMU_H
+#define IMU_H
+
+#include <Wire.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_BNO055.h>
+#include <utility/imumaths.h>
+
+class IMU {
+public:
+    IMU();
+    bool setup();
+    void calibrate();
+    float getHeading();
+    void update();
+
+private:
+    Adafruit_BNO055 _bno;
+    float _heading_offset;
+    float _current_heading;
+};
+
+#endif // IMU_H
---
+++ src/src/core/MotorController.cpp
@@ -0,0 +1,33 @@
+#include "MotorController.h"
+#include "../config.h"
+
+MotorController::MotorController(int dir1_pin, int dir2_pin, int speed_pin) {
+    _dir1_pin = dir1_pin;
+    _dir2_pin = dir2_pin;
+    _speed_pin = speed_pin;
+}
+
+void MotorController::setup() {
+    pinMode(_dir1_pin, OUTPUT);
+    pinMode(_dir2_pin, OUTPUT);
+    pinMode(_speed_pin,OUTPUT);
+    stop(); // Ensure motors are off at start
+}
+
+void MotorController::forward(int speed) {
+    digitalWrite(_dir1_pin, LOW);
+    digitalWrite(_dir2_pin, HIGH);
+    analogWrite(_speed_pin, FORWARD_SPEED);
+}
+
+void MotorController::backward(int speed) {
+    digitalWrite(_dir1_pin, HIGH);
+    digitalWrite(_dir2_pin, LOW);
+    analogWrite(_speed_pin, FORWARD_SPEED);
+}
+
+void MotorController::stop() {
+    digitalWrite(_dir1_pin, LOW);
+    digitalWrite(_dir2_pin, LOW);
+    analogWrite(_speed_pin, 0);
+}
