f047c199aa3c4049ca938cadba33a7bbc8733529
Teto <teto75928@gmail.com>
2025-06-21 17:29:33 +0300

    Ignore .pio folder

--- src/.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.d
+++
@@ -1,4 +0,0 @@
-.pio/build/esp32dev/FrameworkArduino/USBMSC.cpp.o: \
- /home/mega/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.cpp \
- /home/mega/.platformio/packages/framework-arduinoespressif32/cores/esp32/USBMSC.h \
- /home/mega/.platformio/packages/framework-arduinoespressif32/tools/sdk/esp32/dio_qspi/include/sdkconfig.h
